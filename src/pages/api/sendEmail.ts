aaf9a87c2cc96d6774138dbc5cfd1d0bd7ed5ceb
Nizvan MR <njr01397@gmail.com>
2025-03-18 20:35:59 -0600

    Feat: Add ViewTransitions

--- src/pages/api/sendEmail.ts
+++ src/pages/api/sendEmail.ts
@@ -7,23 +7,27 @@ import { Resend } from "resend";
 const resend = new Resend(import.meta.env.RESEND_KEY);
 
 export const POST: APIRoute = async ({ params, request }) => {
-    const { name, email, message } = await request.json();
+    try {
+        const { name, email, message } = await request.json();
 
-    const { data, error } = await resend.emails.send({
-        from: 'Acme <onboarding@resend.dev>',
-        to: ['nizvan.dev@gmail.com'],
-        subject: 'Hello world',
-        react: EmailTemplate({
-            name,
-            email,
-            message
-        }),
-        text: ""
-    });
+        const { data, error } = await resend.emails.send({
+            from: 'Acme <onboarding@resend.dev>',
+            to: ['nizvan.dev@gmail.com'],
+            subject: 'Hello world',
+            react: EmailTemplate({
+                name,
+                email,
+                message
+            }),
+            text: ""
+        });
 
-    if (error) {
+        if (error) {
+            return new Response(JSON.stringify(error), { status: 500 });
+        }
+
+        return new Response(JSON.stringify(data), { status: 200 });
+    } catch (error) {
         return new Response(JSON.stringify(error), { status: 500 });
     }
-
-    return new Response(JSON.stringify(data), { status: 200 });
 }
