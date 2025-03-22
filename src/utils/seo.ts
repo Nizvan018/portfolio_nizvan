60a4e53ab4cfc95d12772503eaa7866ec213c974
Nizvan MR <njr01397@gmail.com>
2025-03-22 10:37:27 -0600

    Feat(Main layout): Make the og and twitter tags a different component

---
+++ src/utils/seo.ts
@@ -0,0 +1,11 @@
+export const seoConfig = {
+    site: "https://nizvan-dev.vercel.app",
+    type: "website",
+    title: "<nizvan /> portafolio web",
+    site_name: "nizvan-dev",
+    description: "Portafolio de desarrollo web Full Stack de Nizvan. Descubre mis proyectos, habilidades y experiencia en aplicaciones web.",
+    image: "https://nizvan-dev.vercel.app/og.jpg",
+    locale: "es_MX",
+    card: "summary_large_image",
+    creator: "@njr01397"
+}
